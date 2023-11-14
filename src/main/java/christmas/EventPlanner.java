ec8d54a62c6b80ff31cfcf4255624549831da3e6
Hyewon Seok <hws2008@naver.com>
2023-11-15 00:13:37 +0900

    feat: 입출력 관리 기능 추가

--- src/main/java/christmas/EventPlanner.java
+++ src/main/java/christmas/EventPlanner.java
@@ -1,2 +1,16 @@
-package christmas;public class EventPlanner {
+package christmas;
+
+public class EventPlanner {
+
+    public void planner() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        InputView inputView = new InputView();
+
+        Order order = new Order(inputView.readDate(), inputView.readOrder());
+
+        System.out.println("12월 3일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        Event event = new Event(order);
+        OutputView outputView = new OutputView(event);
+        outputView.print();
+    }
 }
